f830f6f33d329824425136c2f5d13a1503769eef
Michael Tran <michael.tran@ausloans.com.au>
2023-02-28 16:36:40 +1000

    remove local

---
+++ sonar.bat
@@ -0,0 +1,5 @@
+sonar-scanner.bat \
+  -D"sonar.organization=michaeltran33" \
+  -D"sonar.projectKey=michaeltran33_JenkinsAndSonarcloud" \
+  -D"sonar.sources=." \
+  -D"sonar.host.url=https://sonarcloud.io"
